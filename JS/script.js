5c4a77a3cd5680f07c2f3ee306410c32c6ffea35
Charlesz <126286727+Shidorato037@users.noreply.github.com>
2026-04-17 15:25:37 -0300

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,4 @@
+/* Validação de informções de contato para redirecionar para o contato do watts*/
 const whatsappNumber = '5531986520585';
 
 const contactForm = document.getElementById('contactForm');
@@ -9,22 +10,23 @@ if (contactForm) {
         const lastName = document.getElementById('lastName')?.value.trim() || '';
         const email = document.getElementById('email')?.value.trim() || '';
         const phone = document.getElementById('phone')?.value.trim() || '';
-        const message = document.getElementById('message')?.value.trim() || '';
+        const message = document.getElementById('message')?.value.trim() || ''; /*Pega a id de cada input e valida informações de campos obrigatorios */
 
         const text = `Olá, meu nome é ${firstName} ${lastName}.\n` +
             `E-mail: ${email}\n` +
             `Telefone: ${phone}\n\n` +
-            `Mensagem:\n${message}`;
+            `Mensagem:\n${message}`; /* texto que será enviado no watts*/
 
         const whatsappUrl = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(text)}`;
         window.open(whatsappUrl, '_blank');
     });
 }
-
+/* botão "MEU BOTÃO" de direcionamento para pagina SOBRE*/
 document.getElementById("meuBotao")?.addEventListener('click', () => {
     window.location.href = "./sobre.html"; // URL de destino
 });
-
+/* Evento DOMContentLoaded (pode ser substituido pelo (window.onLoad) DOMCOntentLoaded e disparado quando o DOM (HTML)
+está pronto e usado para ter uma interatividade rapida no site, e foi utilizado para criar a sidebar HAMBURGUER*/
 document.addEventListener('DOMContentLoaded', () => {
     const menuBtn = document.getElementById('menuToggle');
     const sidebar = document.getElementById('sidebar');
@@ -54,4 +56,4 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 });
-
+/*obs (window.onLoad) disparado apenas quando a pagina está 100% carregada (CSS,IMG etc) usado para efeitos visuais animações etc*/
